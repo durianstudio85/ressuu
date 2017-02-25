424168e2602fd379cf48c710df6ce5b642efd328
orlandcastro <ocojcastro@gmail.com>
2017-02-25 17:44:00 +0800

    Updating Delete Jobs

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -404,18 +404,18 @@ public function deleteJobs($id){
                  'date'=> $inputDate
          ]);                 
 
-        // $userList = DB::table('users')->orderBy('id', 'desc')->get();
+               $userList = DB::table('users')->orderBy('id', 'desc')->get();
 
-        //          foreach ($userList as $users ) {
+                 foreach ($userList as $users ) {
                   
-        //           DB::table('dashboard_timeline')->where([
-        //              'category_id' => $job_id,
-        //           ])->delete();  
+                 DB::table('dashboard_timeline')->where([
+                     'category_id' => $job_id,
+                  ])->delete();  
 
-        //         }
+                 }
 
 
-        DB::table('dashboard_timeline')->delete();
+       // DB::table('dashboard_timeline')->delete();
         
 
         return back();
